4195a8fcb4deb316ed3102a9ac94cb41f80604a9
Lokendra Nath <nathdeveloper25@gmail.com>
2025-11-17 10:47:24 +0530

    create admin page navbar

---
+++ app/admin/AdminDashboardClient.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Navbar from "@/components/Navbar";
+
+const AdminDashboardClient = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+    </div>
+  );
+};
+export default AdminDashboardClient;
---
+++ app/admin/page.tsx
@@ -0,0 +1,19 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import AdminDashboardClient from "./AdminDashboardClient";
+
+const AdminPage = async () => {
+  const user = await currentUser();
+
+  // User is Not LogedIn
+  if (!user) redirect("/");
+
+  const adminEmail = process.env.ADMIN_EMAIL;
+  const userEmail = user.emailAddresses[0]?.emailAddress;
+
+  // User is not the admin
+  if (!adminEmail || userEmail !== adminEmail) redirect("/dashboard");
+
+  return <AdminDashboardClient />;
+};
+export default AdminPage;
