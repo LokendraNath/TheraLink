3c9504831c7187a47f95f176bd5f09193d675d37
Lokendra Nath <nathdeveloper25@gmail.com>
2025-11-19 16:47:00 +0530

    create dashboard section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,18 @@
-import { SignOutButton } from "@clerk/nextjs";
+import ActivityOverview from "@/components/dashboard/ActivityOverview";
+import MainAction from "@/components/dashboard/MainAction";
+import WelcomeSection from "@/components/dashboard/WelcomeSection";
+import Navbar from "@/components/Navbar";
 
 const DashboardPage = () => {
   return (
-    <div>
-      <SignOutButton>Sign Out</SignOutButton>
-    </div>
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainAction />
+        <ActivityOverview />
+      </div>
+    </>
   );
 };
 export default DashboardPage;
--- lib/actions/appointments.ts
+++ lib/actions/appointments.ts
@@ -1,7 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use server";
+import { auth } from "@clerk/nextjs/server";
 import { prisma } from "../prisma";
 
-export async function getAppointment() {
+function transformAppointment(appointment: any) {
+  return {
+    ...appointment,
+    patientName: `${appointment.user.firstName || ""} ${
+      appointment.user.lastName || ""
+    }`.trim(),
+    patientEmail: appointment.user.email,
+    doctorName: appointment.doctor.name,
+    doctorImageUrl: appointment.doctor.imageUrl || "",
+    date: appointment.date.toISOString().split("T")[0],
+  };
+}
+
+export async function getAppointments() {
   try {
     const appointments = await prisma.appointment.findMany({
       include: {
@@ -26,5 +41,65 @@ export async function getAppointment() {
   } catch (error) {
     console.log("Error fetching appointments:", error);
     throw new Error("Failed to fetch appointment");
+  }
+}
+
+export async function getUserAppointments() {
+  try {
+    // get authenticated user from  Clerk
+    const { userId } = await auth();
+    if (!userId) throw new Error("You Must be logged in to view appointment");
+
+    const user = await prisma.user.findUnique({ where: { clerkId: userId } });
+
+    if (!user)
+      throw new Error(
+        "User Not Found. Please ensure your account is properly set up"
+      );
+
+    const appointments = await prisma.appointment.findMany({
+      where: { userId: user.id },
+      include: {
+        user: { select: { firstName: true, lastName: true, email: true } },
+        doctor: { select: { name: true, imageUrl: true } },
+      },
+      orderBy: [{ date: "desc" }, { time: "asc" }],
+    });
+
+    return appointments.map(transformAppointment);
+  } catch (error) {
+    console.error("Error fetching user appointments", error);
+    throw new Error("Failed To fetch user Appoinment");
+  }
+}
+
+export async function getUserAppointmentStats() {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("You must be authenticate");
+
+    const user = await prisma.user.findUnique({ where: { clerkId: userId } });
+    if (!user) throw new Error("User not found");
+
+    // These call is run in parraler, insteat of waiting each other
+    const [totalCount, completedCount] = await Promise.all([
+      prisma.appointment.count({
+        where: { userId: user.id },
+      }),
+      prisma.appointment.count({
+        where: {
+          userId: user.id,
+          status: "COMPLETED",
+        },
+      }),
+    ]);
+
+    return {
+      totalAppointment: totalCount,
+      completedAppointment: completedCount,
+    };
+  } catch (error) {
+    console.error("Error Fetching User appointment stats:", error);
+    return { totalAppointment: 0, completedAppointment: 0 };
   }
 }
